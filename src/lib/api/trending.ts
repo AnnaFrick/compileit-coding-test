8f7693e7f991d5e2e97ce0701d05bfc8fa9a38f7
Anna Frick <ae224pc@student.lnu.se>
2024-03-15 13:01:10 +0100

    Replacing accesstoken

--- src/lib/api/trending.ts
+++ src/lib/api/trending.ts
@@ -2,7 +2,7 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjNmOGY1NzJmZGUwYjI2MmZhOTdhMjJkODI0NmJlOSIsInN1YiI6IjY1ZjI5ZGIzMDZmOTg0MDE2MjQ0NTg3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZuPoNo64FAs1_l_0MpS1CoDy6qv6Q8nWAUhrJSWCKS4`
+      Authorization: `Bearer ${process.env.AUTH_ACCESS_TOKEN}`
     }
   };
   
